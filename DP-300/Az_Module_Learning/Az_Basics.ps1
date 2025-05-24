396da5acd8ce009bb6b82e93cef8d2c9f4efb712
Phani Kumar Bellam Konda <58116374+PhaniKumar228@users.noreply.github.com>
2025-05-24 16:00:40 +0530

    az login

--- DP-300/Az_Module_Learning/Az_Basics.ps1
+++ DP-300/Az_Module_Learning/Az_Basics.ps1
@@ -1,1 +1,8 @@
-Test
+az account clear
+az config set core.enable_broker_on_windows=false
+az login
+az login --tenant id 
+az login --tenant 'ACTENSPROGRAM.onmicrosoft.com'
+
+az account set -s '0ba5f6d8-ddbc-4781-9a6b-2907c9b8bb3f'
+az logout
