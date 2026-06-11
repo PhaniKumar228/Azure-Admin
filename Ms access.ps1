21addd1c01855e2fe92281a43f12ba6f0d504972
Phani Kumar Bellam Konda <58116374+PhaniKumar228@users.noreply.github.com>
2026-06-11 21:08:38 +0530

    Update Ms access.ps1

--- Ms access.ps1
+++ Ms access.ps1
@@ -40,3 +40,45 @@ Write-Host "$RowsAffected row(s) updated."
 $conn.Close()
 
 
+########################
+
+$dbPath = "C:\Data\sample.mdb"
+
+$connString = "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=$dbPath;"
+
+$conn = New-Object System.Data.Odbc.OdbcConnection $connString
+$conn.Open()
+
+$query = "SELECT * FROM YourTable"
+
+$cmd = $conn.CreateCommand()
+$cmd.CommandText = $query
+
+$adapter = New-Object System.Data.Odbc.OdbcDataAdapter $cmd
+$table = New-Object System.Data.DataTable
+$adapter.Fill($table) | Out-Null
+
+$table
+
+$conn.Close()
+
+$dbPath = "C:\Data\sample.mdb"
+
+$access = New-Object -ComObject Access.Application
+$db = $access.DBEngine.OpenDatabase($dbPath)
+
+$recordset = $db.OpenRecordset("SELECT * FROM YourTable")
+
+while (-not $recordset.EOF) {
+    $row = @{}
+    foreach ($field in $recordset.Fields) {
+        $row[$field.Name] = $field.Value
+    }
+    [PSCustomObject]$row
+    $recordset.MoveNext()
+}
+
+$recordset.Close()
+$db.Close()
+$access.Quit()
+``
