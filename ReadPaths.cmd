6856f1ecfa88f799c5d2b79c731eaccd87d3861d
Phani Kumar Bellam Konda <58116374+PhaniKumar228@users.noreply.github.com>
2026-07-18 13:01:45 +0530

    Update ReadPaths.cmd

--- ReadPaths.cmd
+++ ReadPaths.cmd
@@ -1,36 +1,31 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-REM Pass file name as parameter
 set "INPUTFILE=%~1"
 set "OUTPUTFILE=SharePaths.txt"
 
-if "%INPUTFILE%"=="" (
-    echo Usage: ReadPaths.cmd FileName.cmd
-    exit /b 1
-)
+echo Input File: %INPUTFILE%
 
 if not exist "%INPUTFILE%" (
-    echo File not found: %INPUTFILE%
+    echo ERROR: File not found or access denied.
+    pause
     exit /b 1
 )
-
-echo =====================================
-echo Processing: %INPUTFILE%
-echo =====================================
 
 if exist "%OUTPUTFILE%" del "%OUTPUTFILE%"
 
+echo =====================================
+echo Processing %~nx1
+echo =====================================
+
 set FOUND=0
 
-for /f "delims=" %%L in (%INPUTFILE%) do (
+for /f "usebackq delims=" %%L in ("%INPUTFILE%") do (
     echo %%L | findstr "\\\\" >nul
-    if not errorlevel 1 (
-        echo %%L | findstr /o "\\\\" >nul
 
+    if not errorlevel 1 (
         echo %%L
         echo %%L>>"%OUTPUTFILE%"
-
         set FOUND=1
     )
 )
@@ -38,11 +33,11 @@ for /f "delims=" %%L in (%INPUTFILE%) do (
 echo.
 
 if !FOUND! EQU 1 (
-    echo STATUS : DONE
+    echo %~nx1 DONE
     echo Results saved to %OUTPUTFILE%
 ) else (
-    echo STATUS : NO PATH FOUND
-    echo No UNC paths found.>"%OUTPUTFILE%"
+    echo %~nx1 NO_PATH_FOUND
+    echo No UNC paths found>"%OUTPUTFILE%"
 )
 
 pause
